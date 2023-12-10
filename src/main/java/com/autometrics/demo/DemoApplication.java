f659213f0000948d9d4c892ec897ccefd4ce6f6f
James Daly <jamsiedaly@gmail.com>
2023-12-10 18:57:59 +0100

    Enable autometrics

--- src/main/java/com/autometrics/demo/DemoApplication.java
+++ src/main/java/com/autometrics/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.autometrics.demo;
 
+import dev.autometrics.bindings.EnableAutometrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutometrics
 public class DemoApplication {
 
 	public static void main(String[] args) {
